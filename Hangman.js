6159f1134f53981f17b93a6b2e6ff90acdbcc120
Victoria Sokolova <viktoriia.sokolova@nure.ua>
2018-11-27 15:46:54 +0200

    Initial commit with nearly done game

---
+++ Hangman.js
@@ -0,0 +1,79 @@
+class Hangman {
+  constructor(word) {
+    this.word = word.toLowerCase();
+    this.hiddenWord = word.replace(/[а-яa-z]/gi, "_").trim();
+  }
+
+  guessLetter(letter) {
+    if (this.containsLetter(letter)) {
+      for (let i = 0; i < this.word.length; i++) {
+        if (this.word[i] === letter)
+          this.hiddenWord = this.replaceLetterAt(
+            this.hiddenWord,
+            i,
+            this.word[i]
+          );
+      }
+    }
+  }
+
+  containsLetter(letter) {
+    if (this.word.indexOf(letter) !== -1) return true;
+    return false;
+  }
+
+  replaceLetterAt(string, index, newLetter) {
+    return (
+      string.substr(0, index) +
+      newLetter +
+      string.substr(index + newLetter.length)
+    );
+  }
+
+  get getHiddenWord() {
+    return this.hiddenWord.split("").join(" ");
+  }
+}
+
+class Game {
+  constructor(word) {
+    this.fails = 0;
+    this.maxFails = 6;
+    this.steps = 0;
+    this.hangman = new Hangman(word);
+  }
+
+  start() {
+    console.log(`Загаданное слово: \n ${this.hangman.getHiddenWord}`);
+    while (this.fails < this.maxFails && !this.isWordGuessed()) {
+      let letter = prompt("Угадай букву!");
+      this.hangman.guessLetter(letter);
+      if (!this.hangman.containsLetter(letter)) { 
+          this.fails++;
+          console.log(`Не угадал! Осталось ${this.maxFails - this.fails} попыток.`);
+      }
+      else console.log(this.hangman.getHiddenWord);
+      this.steps++;
+    }
+
+    this.isWordGuessed()
+      ? console.log(this.successEndGame())
+      : console.log(this.failEndGame());
+  }
+
+  isWordGuessed() {
+    if (this.hangman.hiddenWord.indexOf("_") === -1) return true;
+    return false;
+  }
+
+  successEndGame() {
+    return `Поздравляю, ты победил всего за ${this.steps} шага(ов)!`;
+  }
+
+  failEndGame() {
+    return `Прости, ты сделал ${this.fails} ошибок и проиграл :(`;
+  }
+}
+
+let game = new Game("синхрофазотрон");
+game.start();
